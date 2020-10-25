7f986df51d0575e633139a915b49e53af5e36330
Cecelia Peterson <cecelia@royal-29.cs.wisc.edu>
2020-10-25 13:30:32 -0500

    Added commenting for methods

--- buildgraph.c
+++ buildgraph.c
@@ -1,3 +1,26 @@
 /* represents dependencies between build specifications
 * traverses the graph in bottom-up order
 */
+
+/*
+* build the graph that represents dependencies between build specifications
+*
+* ERROR IF THERE IS A CYCLE
+*/
+void buildGraph() {
+
+}
+
+/*
+* traverse the graph in a bottom-up order to evaluate the specificiations
+* using a post-order traversal (recursion)
+*/
+void traverseGraph() {
+
+    // visit left child
+
+    // visit right child
+
+    // visit current node (root)
+
+}
