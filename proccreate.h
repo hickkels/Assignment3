9a0e20bd17affe98d3b0a35a64d297f916315d62
hickkels <hickok83@yahoo.com>
2020-11-02 19:36:56 -0600

    .

--- proccreate.h
+++ proccreate.h
@@ -3,7 +3,7 @@
 #ifndef PROC_CREATE_H_   /* Include guard */
 #define PROC_CREATE_H_
 
-void run_command(Target *target, Target *target_array);
+void run_command(Target *target, Target *target_array, int num_targets);
 int create_process(char *argv[]);
 
 #endif // PROC_CREATE_H
